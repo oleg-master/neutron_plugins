3f53ad15b1c171488b085a41a0db4c341f6e3b78
Oleg <glavvrach@ngs.ru>
2010-10-01 10:12:05 +0700

    Change format of code and add groupchat exists check

--- seen.py
+++ seen.py
@@ -3,26 +3,29 @@
 import re
 
 def handler_seen(type,source,parameters):
-    groupchat = get_groupchat(source)
-    if parameters:
-        if re.match('wasd22', parameters):
-            msg(groupchat, u'A po4emu vi sprashivaete?')
-        elif re.match('maxally', parameters):
-            msg(groupchat, u'!maxally')
-        elif GROUPCHATS[groupchat].has_key(parameters):
-            msg(groupchat, u'Ya ego vizhu! O_O')
-        else:
-            msg(groupchat, u'Netu ego.')
-    else:
-        msg(groupchat, u'Kakie vashi dokazatelstva?!')
+	groupchat = get_groupchat(source)
+	if not groupchat:
+		return
+	if parameters:
+		if re.match('wasd22', parameters):
+			msg(groupchat, u'A po4emu vi sprashivaete?')
+		elif re.match('maxally', parameters):
+			msg(groupchat, u'!maxally')
+		elif GROUPCHATS[groupchat].has_key(parameters):
+			msg(groupchat, u'Ya ego vizhu! O_O')
+		else:
+			msg(groupchat, u'Netu ego.')
+	else:
+		msg(groupchat, u'Kakie vashi dokazatelstva?!')
         
         
 def handler_bor2(type,source,parameters):
-    groupchat = get_groupchat(source)
-    msg(groupchat, u'[:||||:]')
+	groupchat = get_groupchat(source)
+	if groupchat:
+		msg(groupchat, u'[:||||:]')
     
 def handler_leave_seen(groupchat, nick):
-    msg(groupchat, u'Ushol '+nick);
+	msg(groupchat, u'Ushol '+nick);
     
 
 register_command_handler(handler_seen,u'!seen',0,u'seen',u'!seen',[u'!seen'])
